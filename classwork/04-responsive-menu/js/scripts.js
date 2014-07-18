2e3c2489a3d19d6210003b73d5086caa699d8856
Elissa Torgeson <etorge01@seattlecentral.edu>
2014-07-18 00:17:58 +0000

    updated responsive nav

---
+++ classwork/04-responsive-menu/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("toggleMenu").css("display", "none");
+  
+  $(".nav li").hover(function() {
+     $(this).addClass("hover");
+  }, function() {
+     $(this).removeClass("hover");
+  });
+    
+});
