5cc0c8c6c0aa6202da7b128fb9123484eb039e27
Elissa Torgeson <etorge01@seattlecentral.edu>
2014-07-18 01:14:22 +0000

    final menu tweaks

--- classwork/04-responsive-menu/js/scripts.js
+++ classwork/04-responsive-menu/js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   var ww = document.body.clientWidth;
+    
+  $(".toggleMenu").click(function(e) {
+    e.preventDefault();
+    $(".nav").toggle();
+  });
   
   $(".nav li a").each(function()  {
     if ( $(this).next().length > 0 )  {
@@ -7,18 +12,20 @@ $(document).ready(function(){
     }
   });
   
-  if (ww < 800) {
-    $(".toggleMenu").css("display", "inline-block");
-    $(".nav li a").click(function()  {
-      $(this).parent("li").toggleClass("hover");
-    });
-  } else { 
-    $("toggleMenu").css("display", "none");
+    if (ww < 800) {
+      $(".toggleMenu").css("display", "inline-block");
+      $(".nav").hide();
+      $(".nav li a").click(function()  {
+        $(this).parent("li").toggleClass("hover");
+      });
+    } else { 
+      $("toggleMenu").css("display", "none");
 
-    $(".nav li").hover(function() {
-       $(this).addClass("hover");
-    }, function() {
-       $(this).removeClass("hover");
-    });    
-  }   
-});
+      $(".nav li").hover(function() {
+         $(this).addClass("hover");
+      }, function() {
+         $(this).removeClass("hover");
+      }); 
+    } 
+    
+});//end document.ready function
